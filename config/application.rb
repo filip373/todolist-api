b6859049d439e43ace7ffc7c3dabd944e4239876
Filip Soszyński <filip.soszynski@netguru.co>
2018-01-31 20:04:57 +0100

    Add figaro secret_key_base to app config

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,7 @@ module TodolistApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.secret_key_base = ENV['secret_key_base']
   end
 end
