f542a68a3fadf3b3561e90384cd6d5c47ea6592f
Filip Soszyński <filip.soszynski@netguru.co>
2018-02-17 17:58:38 +0100

    Add api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :v1 do
-    resources :notes
+  namespace :api do
+    namespace :v1 do
+      resources :notes
+    end
   end
 end
