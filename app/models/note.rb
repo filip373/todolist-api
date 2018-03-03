5b81bdf799fa463c4df3396da360739660b82b0e
Filip Soszyński <filip.soszynski@netguru.co>
2018-03-03 22:39:04 +0100

    Replace Proc.new with proc

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,6 +4,6 @@ class Note < ApplicationRecord
   belongs_to :list, optional: true
 
   validates :content, length: { maximum: 500 }
-  validates :list, presence: true, if: Proc.new { |l| l.list_id }
+  validates :list, presence: true, if: proc { |l| l.list_id }
   validates :title, presence: true, length: { maximum: 30 }
 end
