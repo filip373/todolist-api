fa65ad7ec3e130a3cec5dee538269891f031052b
Filip Soszyński <filip.soszynski@netguru.co>
2018-03-03 22:37:18 +0100

    Add list presence validator to note

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe Note, type: :model do
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_length_of(:title).is_at_most(30) }
   it { is_expected.to validate_length_of(:content).is_at_most(500) }
+
+  context 'when list_id is nil' do
+    it { is_expected.not_to validate_presence_of(:list) }
+  end
+
+  context 'when list_id is NOT nil' do
+    let!(:list) { create :list }
+    before { allow(subject).to receive(:list_id).and_return(list.id) }
+
+    it { is_expected.to validate_presence_of(:list) }
+  end
 end
